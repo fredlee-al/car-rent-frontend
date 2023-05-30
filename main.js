dbc59f5f42b5d6c0e92c3e3c9ca52e0752c73630
fredlee0 <128230178+fredlee0@users.noreply.github.com>
2023-05-30 08:04:45 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+const sr = scrollreveal({
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: true,
+});
+sr.reveal(".text", { delay: 200, origin: "top" });
